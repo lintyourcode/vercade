a794df4a19d2c523549aec037a558f0bac6d5dd6
David <161997989+lintyourcode@users.noreply.github.com>
2024-08-01 19:29:28 -0700

    test: add tests for `gpt-4o-mini`

--- tests/test_friend.py
+++ tests/test_friend.py
@@ -5,18 +5,19 @@ import pytest
 from friendbot.friend import Friend, Message, MessageContext
 
 
+MODELS = ["gpt-4o-mini", "claude-3-5-sonnet-20240620"]
+
+
 dotenv.load_dotenv()
 
 
-@pytest.fixture
-def friend():
-    return Friend(
-        identity="You are Proctor, a sentient, smart and snarky Discord chatbot.",
-    )
-
-
 class TestFriend:
-    def test__call__with_greeting_responds_with_nonempty_message(self, friend: Friend):
+    @pytest.mark.parametrize("llm", MODELS)
+    def test__call__with_greeting_responds_with_nonempty_message(self, llm: str):
+        friend = Friend(
+            identity="You are Proctor, a sentient, smart and snarky Discord chatbot.",
+            llm=llm,
+        )
         responses = friend(
             MessageContext(server="Test Server", channel="general"),
             Message(content="Hello, Proctor", author="Bob#0000"),
@@ -28,7 +29,12 @@ class TestFriend:
         assert isinstance(content, str)
         assert content
 
-    def test__call__multiple_times_remembers_previous_messages(self, friend: Friend):
+    @pytest.mark.parametrize("llm", MODELS)
+    def test__call__multiple_times_remembers_previous_messages(self, llm: str):
+        friend = Friend(
+            identity="You are Proctor, a sentient, smart and snarky Discord chatbot.",
+            llm=llm,
+        )
         context = MessageContext(server="Test Server", channel="general")
         friend(
             context, Message(content="Hi, my favorite color is blue", author="Bob#0000")
