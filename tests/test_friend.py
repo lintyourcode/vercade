fae3bcdc6fa20f7923596904e8250afe20b83807
David <161997989+lintyourcode@users.noreply.github.com>
2024-08-04 19:07:34 -0700

    test: fix call assertions

--- tests/test_friend.py
+++ tests/test_friend.py
@@ -1,6 +1,5 @@
 import dotenv
 import pytest
-from pinecone import Pinecone
 from unittest.mock import AsyncMock, Mock, ANY
 
 from friendbot.friend import Friend
@@ -24,8 +23,8 @@ def social_media():
 
 @pytest.fixture
 def pinecone_index():
-    pinecone_index = Mock(spec=Pinecone)
-    pinecone_index.query = Mock()
+    pinecone_index = Mock()
+    pinecone_index.query = Mock(return_value={"matches": []})
     pinecone_index.upsert = Mock()
     return pinecone_index
 
@@ -48,10 +47,10 @@ class TestFriend:
             return_value=[Message(content="Hello, Proctor", author="Bob#0000")]
         )
         await friend(context)
-        # TODO: Update expected author to the friend's identity
-        assert social_media.send.called_once_with(
-            context, Message(content=ANY, author="Bob#0000")
-        )
+        social_media.send.assert_called_once()
+        assert social_media.send.call_args[0][0].server == "Test Server"
+        assert social_media.send.call_args[0][0].channel == "general"
+        # TODO: Assert author is the friend's identity.
         content = social_media.send.call_args[0][1].content
         assert isinstance(content, str)
         assert content
@@ -71,10 +70,21 @@ class TestFriend:
         )
         social_media.messages = AsyncMock(return_value=[message])
         await friend(context)
-        assert social_media.react.called_once_with(context, message, "👍")
+        social_media.react.assert_called_once_with(ANY, ANY, "👍")
+        assert social_media.react.call_args[0][0].server == "Test Server"
+        assert social_media.react.call_args[0][0].channel == "general"
+        assert (
+            social_media.react.call_args[0][1].content
+            == "Please react to this message with a thumbs up"
+        )
+        assert social_media.react.call_args[0][1].author == "Bob#0000"
 
     @pytest.mark.parametrize("llm", MODELS)
-    async def test__call__saves_memory(self, social_media, llm, pinecone_index):
+    async def test__call__saves_memory(self, mocker, social_media, llm, pinecone_index):
+        datetime = mocker.patch("friendbot.friend.datetime")
+        datetime.now = Mock(
+            return_value=Mock(strftime=Mock(return_value="2024-08-01 00:00:00 UTC"))
+        )
         friend = Friend(
             identity="You are Proctor, a sentient and smart Discord chatbot.",
             llm=llm,
@@ -85,14 +95,32 @@ class TestFriend:
         message = Message(content="Hello, Proctor. I'm Bob.", author="Bob#0000")
         social_media.messages = AsyncMock(return_value=[message])
         await friend(context)
-        assert pinecone_index.upsert.called_once_with(vectors=[(ANY, ANY, ANY)])
+        pinecone_index.upsert.assert_called_with(
+            vectors=[
+                (
+                    ANY,
+                    ANY,
+                    {
+                        "content": ANY,
+                        "created_at": "2024-08-01 00:00:00 UTC",
+                    },
+                )
+            ]
+        )
 
     @pytest.mark.parametrize("llm", MODELS)
     async def test__call__retrieves_memory_from_pinecone(
         self, social_media, llm, pinecone_index
     ):
-        pinecone_index.query = AsyncMock(
-            return_value=[{"content": "Has met Bob", "author": "Bob#0000"}]
+        pinecone_index.query = Mock(
+            return_value={
+                "matches": [
+                    {
+                        "created_at": "2024-07-23 00:00:00 UTC",
+                        "content": "Has met Bob",
+                    }
+                ]
+            }
         )
         friend = Friend(
             identity="You are Proctor, a sentient and smart Discord chatbot.",
@@ -104,4 +132,4 @@ class TestFriend:
         message = Message(content="Hello, Proctor. I'm Bob.", author="Bob#0000")
         social_media.messages = AsyncMock(return_value=[message])
         await friend(context)
-        assert pinecone_index.query.called_once_with(query_embedding=ANY, top_k=ANY)
+        pinecone_index.query.assert_called_with(vector=ANY, top_k=ANY)
